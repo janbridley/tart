77f50952db95bcbb5c27129f683d71d915d7caf7
janbridley <bradleyjenj@gmail.com>
2026-08-17 14:47:08 -0400

    system prompt

--- tart-ai/src/openai.rs
+++ tart-ai/src/openai.rs
@@ -2,7 +2,10 @@
 
 use std::{borrow::Cow, path::PathBuf};
 
+pub const SYSTEM: &str = include_str!("data/SYSTEM.md");
+
 mod completions;
+pub use completions::{ChatCompletions, ChatCompletionsClient};
 
 /// Valid `role` entries for a [`Message`]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -20,6 +23,33 @@ struct Message<'a> {
     /// The text associated with this message.
     pub(crate) content: Cow<'a, str>,
 }
+impl<'a> Message<'a> {
+    /// Initialize a `Role::System` message from a markdown file.
+    #[inline]
+    fn from_system_prompt_markdown_file(filename: PathBuf) -> anyhow::Result<Self> {
+        let is_markdown = filename
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .is_some_and(|ext| ext.eq_ignore_ascii_case("md"));
+
+        if !is_markdown {
+            anyhow::bail!("Expected a Markdown file (.md), but received: {filename:?}");
+        }
+
+        Ok(Self {
+            role: Role::System,
+            content: Cow::from(std::fs::read_to_string(&filename)?),
+        })
+    }
+
+    #[inline]
+    fn system() -> Self {
+        Self {
+            role: Role::System,
+            content: Cow::from(SYSTEM),
+        }
+    }
+}
 
 /// Container of history for an LLM session.
 pub struct ContextHistory<'a> {
@@ -32,33 +62,18 @@ impl<'a> ContextHistory<'a> {
     fn append_message(&mut self, msg: Message<'a>) {
         self.responses.push(msg);
     }
-
-    #[inline]
-    fn from_system_prompt_file(&mut self, filename: PathBuf) -> anyhow::Result<()> {
-        let is_markdown = filename
-            .extension()
-            .and_then(|ext| ext.to_str())
-            .is_some_and(|ext| ext.eq_ignore_ascii_case("md"));
-
-        if !is_markdown {
-            anyhow::bail!("Expected a Markdown file (.md), but received: {filename:?}");
-        }
-
-        self.append_message(Message {
-            role: Role::System,
-            content: Cow::from(std::fs::read_to_string(&filename)?),
-        });
-        Ok(())
-    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::openai::{ChatCompletions, ChatCompletionsClient, ContextHistory};
+    use std::path::PathBuf;
+
+    use crate::openai::{ChatCompletionsClient, ContextHistory, Message};
 
     #[test]
     fn completions_can_send_message() {
         let client = ChatCompletionsClient::default();
+        let system = Message::system();
         // let messages = ContextHistory::client.create("glm-5.3");
     }
 }
